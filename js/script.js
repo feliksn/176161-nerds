5368eb7419d965d6547b88209fc1482693e31f5e
feliksn <feliksn@outlook.com>
2016-05-15 18:08:00 +0300

    pre-pre-pre finish

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
-var storage = localStorage.getItem("username");
-
 var link = document.querySelector(".contact-info__btn");
 
 var popup = document.querySelector(".popup");
 var close = popup.querySelector(".popup__close");
 
 var form = popup.querySelector("form");
-var login = popup.querySelector("[name=username]");
+var name = popup.querySelector("[name=username]");
 var email = popup.querySelector("[name=email]");
 var text = popup.querySelector("[name=text]");
 
-var storage = localStorage.getItem("login");
+var storage = localStorage.getItem("name");
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.add("contact-info__btn_show");
 
   if (storage) {
-    login.value = storage;
+    name.value = storage;
     email.focus();
   } else {
-    login.focus();
+    name.focus();
   }
 
 });
@@ -32,13 +30,13 @@ close.addEventListener("click", function(event) {
 });
 
 form.addEventListener("submit", function(event) {
-  if (!login.value || !email.value || !text.value) {
+  if (!name.value || !email.value || !text.value) {
     event.preventDefault();
     popup.classList.remove("popup_shake");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("popup_shake");
   } else {
-    localStorage.setItem("login", login.value);
+    localStorage.setItem("name", name.value);
   }
 });
 
